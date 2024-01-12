81181845f90ccd636392c41b36f194ab4d5722a4
jinia hossen <jiniahossen31@gmail.com>
2024-01-12 14:02:14 +0600

    Board of game made

---
+++ src/Board/Board.jsx
@@ -0,0 +1,39 @@
+export default function Board() {
+  return (
+    <>
+      <div>
+        <button className="w-12 h-12 border border-gray-600 m-1 bg-white leading-9 text-lg">
+          X
+        </button>
+        <button className="w-12 h-12 border border-gray-600 m-1 bg-white leading-9 text-lg">
+          X
+        </button>
+        <button className="w-12 h-12 border border-gray-600 m-1 bg-white leading-9 text-lg">
+          X
+        </button>
+      </div>
+      <div>
+        <button className="w-12 h-12 border border-gray-600 m-1 bg-white leading-9 text-lg">
+          X
+        </button>
+        <button className="w-12 h-12 border border-gray-600 m-1 bg-white leading-9 text-lg">
+          X
+        </button>
+        <button className="w-12 h-12 border border-gray-600 m-1 bg-white leading-9 text-lg">
+          X
+        </button>
+      </div>
+      <div>
+        <button className="w-12 h-12 border border-gray-600 m-1 bg-white leading-9 text-lg">
+          X
+        </button>
+        <button className="w-12 h-12 border border-gray-600 m-1 bg-white leading-9 text-lg">
+          X
+        </button>
+        <button className="w-12 h-12 border border-gray-600 m-1 bg-white leading-9 text-lg">
+          X
+        </button>
+      </div>
+    </>
+  );
+}
