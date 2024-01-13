e5edad6f17899a7a68da587abddc4eedbcaad2bf
jinia hossen <jiniahossen31@gmail.com>
2024-01-13 11:43:02 +0600

    lifting  state up method applied for each sqaure value

--- src/Board/Board.jsx
+++ src/Board/Board.jsx
@@ -1,28 +1,35 @@
-function Sqaure() {
+import { useState } from "react";
+
+function Sqaure({ value }) {
+  function handleClick() {}
   return (
-    <button className="w-12 h-12 border border-gray-600 m-1 bg-white leading-9 text-lg">
-      X
+    <button
+      onClick={handleClick} //handleClick is not defined
+      className="w-20 h-20 border border-gray-600 m-1 bg-white leading-9 text-lg"
+    >
+      {value}
     </button>
   );
 }
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
   return (
     <>
-      <div>
-        <Sqaure />
-        <Sqaure />
-        <Sqaure />
+      <div className="flex ">
+        <Sqaure value={squares[1]} />
+        <Sqaure value={squares[2]} />
+        <Sqaure value={squares[3]} />
       </div>
-      <div>
-        <Sqaure />
-        <Sqaure />
-        <Sqaure />
+      <div className="flex ">
+        <Sqaure value={squares[4]} />
+        <Sqaure value={squares[5]} />
+        <Sqaure value={squares[6]} />
       </div>
-      <div>
-        <Sqaure />
-        <Sqaure />
-        <Sqaure />
+      <div className="flex ">
+        <Sqaure value={squares[7]} />
+        <Sqaure value={squares[8]} />
+        <Sqaure value={squares[9]} />
       </div>
     </>
   );
